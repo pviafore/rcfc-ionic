5819e491ef6436cdf0e3c51a1fad65eb49afacf9
Pat Viafore <patviafore@gmail.com>
2018-09-09 22:15:41 -0500

    Modernize build apk.sh

--- build_apk.sh
+++ build_apk.sh
@@ -1,6 +1,6 @@
 rm platforms/android/build/outputs/apk/*.apk
 
 ionic cordova build android --prod --release
-$JAVA_HOME/bin/jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $1 platforms/android/build/outputs/apk/android-release-unsigned.apk my-alias
-$ANDROID_HOME/build-tools/27.0.3/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk platforms/android/build/outputs/apk/RCFC.apk
-$ANDROID_HOME/build-tools/27.0.3/apksigner verify platforms/android/build/outputs/apk/RCFC.apk
+$JAVA_HOME/bin/jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $1 platforms/android/build/outputs/apk/release/android-release-unsigned.apk my-alias
+$ANDROID_HOME/build-tools/28.0.2/zipalign -v 4 platforms/android/build/outputs/apk/release/android-release-unsigned.apk platforms/android/build/outputs/apk/RCFC.apk
+$ANDROID_HOME/build-tools/28.0.2/apksigner verify platforms/android/build/outputs/apk/RCFC.apk
